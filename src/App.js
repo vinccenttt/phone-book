e501d7d0f0411d1e471cffaf36333a17334a90b1
Vincent <vincentgoering@gmail.com>
2024-05-29 22:07:35 +0200

    feat: basic implementation, no styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import phoneBookData from "./phonebook.json";
+
 function App() {
-  return <div>Hallo Welt</div>;
+  const [searchString, setSearchString] = useState("");
+
+  return (
+    <div>
+      <h1>Telefonbuchsuche</h1>
+      <input
+        type="text"
+        onChange={(e) => setSearchString(e.target.value)}
+        placeholder="Max Mustermann"
+      />
+      <div>
+        <SearchResult searchString={searchString} data={phoneBookData} />
+      </div>
+    </div>
+  );
+}
+
+function SearchResult({ searchString, data }) {
+  function reduceString(word) {
+    return word.toLowerCase().replace(/[\s-]/g, "");
+  }
+
+  const reducedSearchString = reduceString(searchString);
+
+  const searchResults = data.filter((e) => {
+    const reducedName = reduceString(e.name);
+    return reducedName.includes(reducedSearchString);
+  });
+
+  return (
+    <>
+      <h3>Suchergebnis für {searchString}</h3>
+      <div>
+        {searchResults.map((e) => (
+          <PersonCard key={e.phone} name={e.name} phone={e.phone} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function PersonCard({ name, phone }) {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{phone}</p>
+    </div>
+  );
 }
 
 export default App;
